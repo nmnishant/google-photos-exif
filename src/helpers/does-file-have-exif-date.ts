afa105fc61995fb541d4515cf950d39a4b0260a5
Nishant Maurya <74682951+nmnishant@users.noreply.github.com>
2025-04-18 13:21:19 +0530

    change

--- src/helpers/does-file-have-exif-date.ts
+++ src/helpers/does-file-have-exif-date.ts
@@ -1,22 +1,24 @@
-import { exiftool } from 'exiftool-vendored';
 import { isNullOrUndefined } from './is-null-or-undefined';
 import { doesFileSupportExif } from './does-file-support-exif';
-import { promises as fspromises } from "fs";
-import { basename, join, resolve } from 'path';
-const { unlink, copyFile } = fspromises;
+import { promisify } from 'util';
+import { exec } from 'child_process';
+const execAsync = promisify(exec);
 
 export async function doesFileHaveExifDate(filePath: string): Promise<boolean> {
   if (!doesFileSupportExif(filePath)) {
     return false;
   }
 
-  console.log('doesFileHaveExifDate:::', filePath);  
+  console.log('doesFileHaveExifDate:::', filePath);
 
+  try {
+    const { stdout } = await execAsync(`exiftool -DateTimeOriginal -j "${filePath}"`);
+    const exifData = JSON.parse(stdout)[0];
 
-  const newfilePath = resolve('/data/data/com.termux/files/home/temp', basename(filePath));
-  console.log({newfilePath});
-  const readResult = await exiftool.read(newfilePath);
-
-  console.log('readResult:::', readResult)
-  return !isNullOrUndefined(readResult.DateTimeOriginal);
+    console.log('readResult:::', exifData);
+    return !isNullOrUndefined(exifData.DateTimeOriginal);
+  } catch (err) {
+    console.error('ExifTool error:', err.message);
+    return false;
+  }
 }
