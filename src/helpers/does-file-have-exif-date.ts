c6edbbe601f5187a9f2c93d87f37fd5888c498c6
Nishant Maurya <74682951+nmnishant@users.noreply.github.com>
2025-04-18 12:35:20 +0530

    change

--- src/helpers/does-file-have-exif-date.ts
+++ src/helpers/does-file-have-exif-date.ts
@@ -1,6 +1,9 @@
 import { exiftool } from 'exiftool-vendored';
 import { isNullOrUndefined } from './is-null-or-undefined';
-import { doesFileSupportExif } from './does-file-support-exif'
+import { doesFileSupportExif } from './does-file-support-exif';
+import { promises as fspromises } from "fs";
+import { basename, join } from 'path';
+const { unlink, copyFile } = fspromises;
 
 export async function doesFileHaveExifDate(filePath: string): Promise<boolean> {
   if (!doesFileSupportExif(filePath)) {
@@ -9,7 +12,15 @@ export async function doesFileHaveExifDate(filePath: string): Promise<boolean> {
 
   console.log('doesFileHaveExifDate:::', filePath);  
 
-  const readResult = await exiftool.read(filePath);
+  const fileName = basename(filePath);
+  const tempFilePath = join('./temp', fileName);
+
+  await copyFile(
+    filePath,
+    tempFilePath
+  );
+
+  const readResult = await exiftool.read(tempFilePath);
 
   console.log('readResult:::', readResult)
   return !isNullOrUndefined(readResult.DateTimeOriginal);
