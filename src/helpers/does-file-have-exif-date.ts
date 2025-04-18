6088f636309cf913528da72d8356f1828bfafc9a
Nishant Maurya <74682951+nmnishant@users.noreply.github.com>
2025-04-18 12:43:05 +0530

    change

--- src/helpers/does-file-have-exif-date.ts
+++ src/helpers/does-file-have-exif-date.ts
@@ -2,10 +2,12 @@ import { exiftool } from 'exiftool-vendored';
 import { isNullOrUndefined } from './is-null-or-undefined';
 import { doesFileSupportExif } from './does-file-support-exif';
 import { promises as fspromises } from "fs";
-import { basename, join } from 'path';
+import { basename, join, resolve } from 'path';
 const { unlink, copyFile } = fspromises;
 
 export async function doesFileHaveExifDate(filePath: string): Promise<boolean> {
+  filePath = resolve(filePath);
+
   if (!doesFileSupportExif(filePath)) {
     return false;
   }
